0cf23b3d1103fcb1a90d3433645687b5da0cccc5
LeergutLars <119765991+LeergutLarss@users.noreply.github.com>
2023-02-19 23:53:23 +0100

    Add win/lose score JS

--- script.js
+++ script.js
@@ -21,17 +21,22 @@ function getComputerChoice () {
 //Player input
 
 const inputButtons = document.querySelectorAll(".game button");
+const score = [0,0]
 
 inputButtons.forEach(button => {
     button.addEventListener("click", () => {
-        playRound(getComputerChoice(), button.id);
+        playRound(getComputerChoice(), button.id,score);
     });
 });
 
 
-function playRound(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection,score) {
     
     let state;
+
+    const playerOut = document.querySelector("#player");
+    const computerOut = document.querySelector("#computer");
+
     const mainGame = document.querySelector(".game");
     const output = document.querySelector(".status");
 
@@ -43,9 +48,13 @@ function playRound(computerSelection, playerSelection) {
                 switch (computerSelection) {
                     case "Paper":
                         state = "You Lose!";
+                        score[0]++;
+                        computerOut.textContent = `Com: ${score[0]}`;
                         break;
                     case "Scissors":
                         state = "You Win!";
+                        score[1]++;
+                        playerOut.textContent = `You: ${score[1]}`;
                         break;    
                     default:
                         break;
@@ -56,9 +65,13 @@ function playRound(computerSelection, playerSelection) {
                 switch (computerSelection) {
                     case "Rock":
                         state = "You Lose!";
+                        score[0]++;
+                        computerOut.textContent = `Com: ${score[0]}`;
                         break;
                     case "Paper":
                         state = "You Win!";
+                        score[1]++;
+                        playerOut.textContent = `You: ${score[1]}`;
                         break;    
                     default:
                         break;
@@ -69,9 +82,13 @@ function playRound(computerSelection, playerSelection) {
                 switch (computerSelection) {
                     case "Scissors":
                         state = "You Lose!";
+                        score[0]++;
+                        computerOut.textContent = `Com: ${score[0]}`;
                         break;
                     case "Rock":
                         state = "You Win!";
+                        score[1]++;
+                        playerOut.textContent = `You: ${score[1]}`;
                         break;    
                     default:
                         break;
@@ -80,5 +97,11 @@ function playRound(computerSelection, playerSelection) {
                 break;
         }
     }
-    output.textContent = `${state}`;
+    if(score[0] == 5) {
+        output.textContent = "You Lost the Game";
+    } else if(score[1] == 5) {
+        output.textContent = "You Won the Game";
+    } else {
+        output.textContent = `${state}`;
+    }    
 }
